05d6dd03cd922cc1d96b880fb87e751c0499a0e6
mahesh <jadhao.mahesh1009@gmail.com>
2024-03-04 11:41:51 +0530

    first commit

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -49,7 +49,7 @@ export const updateUser = async (req, res, next) => {
       try {
          await User.findByIdAndDelete(req.params.id);
            res.clearCookie('access_token')
-         res.status(200).json('user has been deleted!');
+           res.status(200).json('user has been deleted!');
 
 
       } catch (error) {
@@ -63,13 +63,8 @@ export const updateUser = async (req, res, next) => {
     if(req.user.id === req.params.id){
       
       try {
-        
         const listings = await Listing.find({userRef: req.params.id}) ;
-
         res.status(200).json(listings);
-
-
-
       } catch (error) {
         next(error)
       }
